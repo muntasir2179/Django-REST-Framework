85a6f3d32338164a85de05605b4d16279387beb4
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-01-09 17:58:09 +0600

    framework included in installed app setting

--- app/models.py
+++ app/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=30)
+    email = models.CharField(max_length=30)
+    password = models.CharField(max_length=30)
+    phone = models.CharField(max_length=12)
