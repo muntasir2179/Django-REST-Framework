856d51c9cfd5122e39b01c5cb4d8a6891e7795bf
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-01-10 23:52:09 +0600

    Assignment Error Fixed (request_handle_error)

--- app/views.py
+++ app/views.py
@@ -70,7 +70,7 @@ def employeeDetailView(request, pk):
             return Response(serializer.errors)
 
 
-# Assignment view
+# Assignment works
 @api_view(['GET', 'POST'])
 def courseListView(request):
     if request.method == 'GET':
@@ -78,8 +78,33 @@ def courseListView(request):
         serializer = CourseSerializer(courses, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
+    elif request.method == 'POST':
         serializer = CourseSerializer(data=request.data)
+
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        else:
+            return Response(serializer.errors)
+
+
+@api_view(['GET', 'DELETE', 'PUT'])
+def courseDetailView(request, pk):
+    try:
+        course = Course.objects.get(pk=pk)
+    except Course.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
+
+    if request.method == 'GET':
+        serializer = CourseSerializer(course)
+        return Response(serializer.data)
+
+    elif request.method == 'DELETE':
+        course.delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+    elif request.method == 'PUT':
+        serializer = CourseSerializer(course, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
--- quickstart/urls.py
+++ quickstart/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from app.views import employeeListView, userListView, employeeDetailView, courseListView
+from app.views import employeeListView, userListView, employeeDetailView, courseListView, courseDetailView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,4 +24,5 @@ urlpatterns = [
     path('api/users', userListView),
     # assignment
     path('api/courses', courseListView),
+    path('api/courses/<int:pk>', courseDetailView),
 ]
