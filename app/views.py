1b6c4f659add0e9c3d47e138b8915cb6b299a1e6
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-01-10 19:57:40 +0600

    @api_view() implemented

--- app/views.py
+++ app/views.py
@@ -1,78 +1,70 @@
-from django.http import JsonResponse, HttpResponse
 from .models import Employee
 from .serializers import EmployeeSerializer, UserSerializer
 from django.contrib.auth.models import User
-from django.views.decorators.csrf import csrf_exempt
-from rest_framework.parsers import JSONParser
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def employeeListView(request):
     if request.method == 'GET':
         employees = Employee.objects.all()
         serializer = EmployeeSerializer(employees, many=True)
         # Json response takes dictionary other wise we need to set safe=False to avoid any exception
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'POST':
         # POST request from Postman with some data
-        # JSONParser is parsing the data from request sent by Postman
-        jsonData = JSONParser().parse(request)
-        serializer = EmployeeSerializer(data=jsonData)
-
-        # showing the data in the consol
-        print(jsonData)
+        serializer = EmployeeSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors, safe=False)
+            return Response(serializer.errors)
 
 
-@csrf_exempt
+@api_view(['GET', 'POST'])
 def userListView(request):
     if request.method == 'GET':
         users = User.objects.all()
         serializer = UserSerializer(users, many=True)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     if request.method == 'POST':
-        jsonData = JSONParser().parse(request)
-        serializer = UserSerializer(data=jsonData)
+        serializer = UserSerializer(data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors, safe=False)
+            return Response(serializer.errors)
 
 
-@csrf_exempt
+@api_view(['GET', 'PUT', 'DELETE'])
 def employeeDetailView(request, pk):
     try:
         employee = Employee.objects.get(pk=pk)
     except Employee.DoesNotExist:
-        return HttpResponse(status=404)
+        return Response(status=404)
 
     if request.method == 'GET':
         serializer = EmployeeSerializer(employee)
-        return JsonResponse(serializer.data, safe=False)
+        return Response(serializer.data)
 
     elif request.method == 'DELETE':
         # delete request
         employee.delete()
-        return HttpResponse("Employee data deleted successfully....")
+        return Response("Employee data deleted successfully....")
 
     elif request.method == 'PUT':
         # update request
-        jsonData = JSONParser().parse(request)
-        serializer = EmployeeSerializer(employee, data=jsonData)
+        serializer = EmployeeSerializer(employee, data=request.data)
 
         if serializer.is_valid():
             serializer.save()
-            return JsonResponse(serializer.data, safe=False)
+            return Response(serializer.data)
         else:
-            return JsonResponse(serializer.errors, safe=False)
+            return Response(serializer.errors)
