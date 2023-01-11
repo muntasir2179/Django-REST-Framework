ec871e471d8cfc2e612247170b38edbab7576011
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-01-11 21:02:27 +0600

    mixins and generics class implemented

--- app/views.py
+++ app/views.py
@@ -6,115 +6,40 @@ from rest_framework.decorators import api_view
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from django.http import Http404
+from rest_framework import mixins, generics
 # Create your views here.
 
 
-@api_view(['GET', 'POST'])
-def employeeListView(request):
-    if request.method == 'GET':
-        employees = Employee.objects.all()
-        serializer = EmployeeSerializer(employees, many=True)
-        # Json response takes dictionary other wise we need to set safe=False to avoid any exception
-        return Response(serializer.data)
+class CourseAllView(mixins.ListModelMixin, mixins.CreateModelMixin, generics.GenericAPIView):
+    # the list() method is in the ListModelMixin
+    # the create() method is in the CreateModelMixin
+    # the request methods are in GenericAPIView
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    elif request.method == 'POST':
-        # POST request from Postman with some data
-        serializer = EmployeeSerializer(data=request.data)
+    def get(self, request):
+        return self.list(request)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+    def post(self, request):
+        return self.create(request)
 
 
-@api_view(['GET', 'POST'])
-def userListView(request):
-    if request.method == 'GET':
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
+class CourseAllViewWithId(generics.GenericAPIView, mixins.RetrieveModelMixin, mixins.UpdateModelMixin, mixins.DestroyModelMixin):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
 
-    if request.method == 'POST':
-        serializer = UserSerializer(data=request.data)
+    def get(self, request, pk):
+        return self.retrieve(request, pk)
 
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
+    def put(self, request, pk):
+        return self.update(request, pk)
+
+    def delete(self, request, pk):
+        return self.destroy(request, pk)
 
 
-@api_view(['GET', 'PUT', 'DELETE'])
-def employeeDetailView(request, pk):
-    try:
-        employee = Employee.objects.get(pk=pk)
-    except Employee.DoesNotExist:
-        return Response(status=404)
-
-    if request.method == 'GET':
-        serializer = EmployeeSerializer(employee)
-        return Response(serializer.data)
-
-    elif request.method == 'DELETE':
-        # delete request
-        employee.delete()
-        return Response("Employee data deleted successfully....")
-
-    elif request.method == 'PUT':
-        # update request
-        serializer = EmployeeSerializer(employee, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-
+'''
 # Assignment works
-@api_view(['GET', 'POST'])
-def courseListView(request):
-    if request.method == 'GET':
-        courses = Course.objects.all()
-        serializer = CourseSerializer(courses, many=True)
-        return Response(serializer.data)
-
-    elif request.method == 'POST':
-        serializer = CourseSerializer(data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        else:
-            return Response(serializer.errors)
-
-
-@api_view(['GET', 'DELETE', 'PUT'])
-def courseDetailView(request, pk):
-    try:
-        course = Course.objects.get(pk=pk)
-    except Course.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
-
-    if request.method == 'GET':
-        serializer = CourseSerializer(course)
-        return Response(serializer.data)
-
-    elif request.method == 'DELETE':
-        course.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
-    elif request.method == 'PUT':
-        serializer = CourseSerializer(course, data=request.data)
-
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        else:
-            return Response(serializer.errors)
-
-
 # class based view
 class CourseAllView(APIView):
     def get(self, request):
@@ -154,3 +79,4 @@ class CourseAllViewWithId(APIView):
             return Response(serializer.data)
         else:
             return Response(serializer.errors)
+'''
