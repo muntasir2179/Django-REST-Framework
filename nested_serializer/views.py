d001339746d58f6127cfb1cc0f0e7b80259d27e5
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-01-14 14:55:23 +0600

    Custom Permission applied

--- nested_serializer/views.py
+++ nested_serializer/views.py
@@ -1,9 +1,20 @@
 from rest_framework import generics
 from .serializers import InstructorSerializer, CourseSerializer
 from .models import Instructor, Course
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, IsAdminUser, BasePermission
 from rest_framework.authentication import BasicAuthentication
 # Create your views here.
+
+
+class WriteByAdminOnlyPermission(BasePermission):
+    def has_permission(self, request, view):
+        user = request.user
+        if request.method == 'GET':
+            return True
+        if request.method == 'POST' or request.method == 'PUT' or request.method == 'DELETE':
+            if user.is_superuser:
+                return True
+        return False
 
 
 class InstructorListView(generics.ListCreateAPIView):
@@ -11,7 +22,7 @@ class InstructorListView(generics.ListCreateAPIView):
     authentication_classes = [BasicAuthentication]
     # we must be logged in to the admin site to have our login data into the session
     # otherwise we will not get access to the api requested data
-    permission_classes = [IsAuthenticated]
+    permission_classes = [WriteByAdminOnlyPermission]
     serializer_class = InstructorSerializer
     queryset = Instructor.objects.all()
 
