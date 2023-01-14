362ae053e2b0aaf61c5852fe5dbee92b930ce4fd
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-01-14 14:41:21 +0600

    BasicAuthentication applied

--- nested_serializer/views.py
+++ nested_serializer/views.py
@@ -2,30 +2,36 @@ from rest_framework import generics
 from .serializers import InstructorSerializer, CourseSerializer
 from .models import Instructor, Course
 from rest_framework.permissions import IsAuthenticated
+from rest_framework.authentication import BasicAuthentication
 # Create your views here.
 
 
 class InstructorListView(generics.ListCreateAPIView):
+    # authentication through username and password
+    authentication_classes = [BasicAuthentication]
+    # we must be logged in to the admin site to have our login data into the session
+    # otherwise we will not get access to the api requested data
     permission_classes = [IsAuthenticated]
     serializer_class = InstructorSerializer
     queryset = Instructor.objects.all()
 
 
 class InstructorDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = InstructorSerializer
     queryset = Instructor.objects.all()
 
 
 class CourseListView(generics.ListCreateAPIView):
-    # we must be logged in to the admin site to have our login data into the session
-    # otherwise we will not get access to the api requested data
+    authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
 
 
 class CourseDetailView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [BasicAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = CourseSerializer
     queryset = Course.objects.all()
