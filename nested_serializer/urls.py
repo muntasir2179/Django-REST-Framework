30067340c65ff90937970f5aeb61f7176c102ea0
Muntasir Ahmed <muntasirahmed2179@gmail.com>
2023-01-14 10:49:07 +0600

    HyperlinkedModelSerializer implemented

--- nested_serializer/urls.py
+++ nested_serializer/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import InstructorListView, CourseListView
+from .views import InstructorListView, InstructorDetailView, CourseListView, CourseDetailView
 
 
 urlpatterns = [
     path('instructors', InstructorListView.as_view()),
+    path('instructors/<int:pk>', InstructorDetailView.as_view(),
+         name='instructor-detail'),
     path('courses', CourseListView.as_view()),
+    path('courses/<int:pk>', CourseDetailView.as_view(), name='course-detail'),
 ]
