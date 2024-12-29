79015a90fa0c67f4dbf2db0297020082fa5ba1ae
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-29 22:52:43 +0600

    overridden the create and update method in MovieSerializer class to perform create and update logic and also enabled more methods for each function views

--- watchlist_app/api/serializers.py
+++ watchlist_app/api/serializers.py
@@ -1,8 +1,39 @@
 from rest_framework import serializers
-
+from watchlist_app.models import Movie
 
 class MovieSerializer(serializers.Serializer):
     id = serializers.IntegerField(read_only=True)
     name = serializers.CharField()
     description = serializers.CharField()
     active = serializers.BooleanField()
+
+    # overriding the create method to perform dat insertion logic
+    def create(self, validated_data):
+        """_summary_
+
+        Args:
+            validated_data (object): deserialized data which is sent through the api request
+
+        Returns:
+            object: a model object created using the data sent through the api request
+        """
+        return Movie.objects.create(**validated_data)
+
+
+    # overriding the update method to perform data update logic
+    def update(self, instance, validated_data):
+        """_summary_
+
+        Args:
+            instance (object): old data
+            validated_data (object): updated data sent through api request
+
+        Returns:
+            object: updated model object data
+        """
+        instance.name = validated_data.get('name', instance.name)
+        instance.description = validated_data.get('description', instance.description)
+        instance.active = validated_data.get('active', instance.active)
+        instance.save()
+        return instance
+
--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -6,15 +6,39 @@ from rest_framework.decorators import api_view
 # Create your views here.
 
 
-@api_view()
+@api_view(['GET', 'POST'])  # enabling more request methods for this view
 def movie_list(request):
-    movies = Movie.objects.all()
-    serializer = MovieSerializer(movies, many=True)   # When serializer have to go through more than one object we have to set 'many=True'
-    return Response(serializer.data)
+    if request.method == "GET":
+        movies = Movie.objects.all()
+        serializer = MovieSerializer(movies, many=True)   # When serializer have to go through more than one object we have to set 'many=True'
+        return Response(serializer.data)
+    
+    if request.method == "POST":
+        serializer = MovieSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
 
 
-@api_view()
+@api_view(['GET', 'PUT', 'DELETE'])   # enabling more request methods for this view
 def movie_details(request, pk):
-    movie = Movie.objects.get(pk=pk)
-    serializer = MovieSerializer(movie)   # Here we are not setting 'many=True' because serializer only have to go through one object
-    return Response(serializer.data)
+    if request.method == 'GET':
+        movie = Movie.objects.get(pk=pk)
+        serializer = MovieSerializer(movie)   # Here we are not setting 'many=True' because serializer only have to go through one object
+        return Response(serializer.data)
+    
+    if request.method == 'PUT':
+        movie = Movie.objects.get(pk=pk)   # we need to select the exact model object that we want to update and we have to pass that into the serializer alongside the data
+        serializer = MovieSerializer(movie, data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data)
+        else:
+            return Response(serializer.errors)
+    
+    if request.method == 'DELETE':
+        movie = Movie.objects.get(pk=pk)
+        movie.delete()
+        return Response(data={"message": "Data is successfully deleted."})
