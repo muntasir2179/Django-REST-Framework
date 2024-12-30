1887ed3abe442f973be781f3ec8b00971f04b4df
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-30 15:05:57 +0600

    added status code in the response

--- watchlist_app/api/views.py
+++ watchlist_app/api/views.py
@@ -2,6 +2,7 @@ from watchlist_app.models import Movie
 from .serializers import MovieSerializer
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
+from rest_framework import status
 
 # Create your views here.
 
@@ -9,9 +10,12 @@ from rest_framework.decorators import api_view
 @api_view(['GET', 'POST'])  # enabling more request methods for this view
 def movie_list(request):
     if request.method == "GET":
-        movies = Movie.objects.all()
+        try:
+            movies = Movie.objects.all()
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found.'}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movies, many=True)   # When serializer have to go through more than one object we have to set 'many=True'
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     if request.method == "POST":
         serializer = MovieSerializer(data=request.data)
@@ -25,9 +29,12 @@ def movie_list(request):
 @api_view(['GET', 'PUT', 'DELETE'])   # enabling more request methods for this view
 def movie_details(request, pk):
     if request.method == 'GET':
-        movie = Movie.objects.get(pk=pk)
+        try:
+            movie = Movie.objects.get(pk=pk)
+        except Movie.DoesNotExist:
+            return Response({'error': 'Movie not found.'}, status=status.HTTP_404_NOT_FOUND)
         serializer = MovieSerializer(movie)   # Here we are not setting 'many=True' because serializer only have to go through one object
-        return Response(serializer.data)
+        return Response(serializer.data, status=status.HTTP_200_OK)
     
     if request.method == 'PUT':
         movie = Movie.objects.get(pk=pk)   # we need to select the exact model object that we want to update and we have to pass that into the serializer alongside the data
@@ -36,9 +43,9 @@ def movie_details(request, pk):
             serializer.save()
             return Response(serializer.data)
         else:
-            return Response(serializer.errors)
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
     if request.method == 'DELETE':
         movie = Movie.objects.get(pk=pk)
         movie.delete()
-        return Response(data={"message": "Data is successfully deleted."})
+        return Response({"error": "Data is successfully deleted."}, status=status.HTTP_204_NO_CONTENT)
