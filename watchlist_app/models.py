a9c30e5f52f017edee2b06b2884cd2577299eaae
Muntasir2179 <muntasirahmed2179@gmail.com>
2024-12-31 16:06:38 +0600

    applied one-to-many relationship and applied nested serializer

--- watchlist_app/models.py
+++ watchlist_app/models.py
@@ -16,6 +16,7 @@ class WatchList(models.Model):
     storyline = models.CharField(max_length=200)
     active = models.BooleanField(default=True)
     created = models.DateTimeField(auto_now_add=True)
+    platform = models.ForeignKey(StreamPlatform, on_delete=models.CASCADE, related_name="watchlist")   # establishing a one-to-many relationship
     
     def __str__(self):
         return self.title
