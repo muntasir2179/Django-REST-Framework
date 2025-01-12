5428bb3dcc63331f5debfcf6241f0de40b4bf74c
Muntasir2179 <muntasirahmed2179@gmail.com>
2025-01-12 15:38:39 +0600

    written test case for login and logout in user_app

--- user_app/tests.py
+++ user_app/tests.py
@@ -20,3 +20,28 @@ class RegisterTestCase(APITestCase):
         response = self.client.post(reverse('register'), data=data)
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
 
+
+class LoginLogoutTestCase(APITestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username='testcase', password='testpassword')
+    
+    
+    def test_login(self):
+        data = {
+            "username": "testcase",
+            "password": "testpassword"
+        }
+        
+        response = self.client.post(reverse('login'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+
+    def test_logout(self):
+        # we have to login and get the access token to perform logout
+        self.token = Token.objects.get_or_create(user=self.user)[0]
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        # sending a logout request
+        response = self.client.post(reverse('logout'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
