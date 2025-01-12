63a40631aaf6a9d859c9d70392e2b716c2903c1d
Muntasir2179 <muntasirahmed2179@gmail.com>
2025-01-13 00:15:37 +0600

    written test cases for watchlist endpoints

--- watchlist_app/tests.py
+++ watchlist_app/tests.py
@@ -11,6 +11,7 @@ from . import models
 # Create your tests here.
 
 
+# test cases for streamplatform
 class StreamPlatformTestCase(APITestCase):
     def setUp(self):
         # creating a test user
@@ -61,3 +62,69 @@ class StreamPlatformTestCase(APITestCase):
         response = self.client.delete(reverse('streamplatform-detail', args=(self.stream.id,)))
         self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
+
+
+# test cases for watchlist
+class WatchListTestCase(APITestCase):
+    def setUp(self):
+        # creating a test user
+        self.user = User.objects.create_user(username="testcase", password="testpassword")
+        self.token = Token.objects.get_or_create(user=self.user)[0]
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token.key)
+        
+        # creating a platform data
+        self.stream = models.StreamPlatform.objects.create(name="Netflix", about="#1 Platform", website="https://www.netflix.com")
+        
+        # creating a watchlist
+        self.watchlist = models.WatchList.objects.create(
+            platform=self.stream,
+            title="Example Movie",
+            storyline="This is a good movie!",
+            active=True
+        )
+    
+    
+    # test case for creating stream platform as a normal user
+    def test_watchlist_create(self):
+        data = {
+            "title": "Marvel",
+            "storyline": "This is a good movie!",
+            "active": True,
+            "platform": self.stream.id
+        }
+        response = self.client.post(reverse('watch-list'), data=data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    
+    # test for retrieving watchlist as a normal user
+    def test_watchlist_list(self):
+        response = self.client.get(reverse('watch-list'))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    
+    # test for retrieving individual watchlist as a normal user
+    def test_watchlist_ind(self):
+        response = self.client.get(reverse('watch-details', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+        # we can check for other values and objects as well
+        self.assertEqual(models.WatchList.objects.count(), 1)
+        self.assertEqual(models.WatchList.objects.get().title, 'Example Movie')
+
+    
+    # test for updating individual watchlist as a normal user
+    def test_watchlist_update(self):
+        data = {
+            "title": "Marvel",
+            "storyline": "This is a good movie!",
+            "active": True,
+            "platform": self.stream.id
+        }
+        response = self.client.put(reverse('watch-details', args=(self.watchlist.id,)), data=data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+
+    # test for deleting individual watchlist as a normal user
+    def test_watchlist_delete(self):
+        response = self.client.delete(reverse('watch-details', args=(self.watchlist.id,)))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
